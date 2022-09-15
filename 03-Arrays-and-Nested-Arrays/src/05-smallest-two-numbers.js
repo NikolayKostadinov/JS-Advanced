1d063dc50837f89fa93207ce68139985be8b0098
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-15 15:00:17 +0300

    add 05-smallest-two-numbers.js

---
+++ 03-Arrays-and-Nested-Arrays/src/05-smallest-two-numbers.js
@@ -0,0 +1,6 @@
+function smallestTwoNumbers(arr){
+    console.log(arr.sort((a,b) => Number(a) -Number(b)).splice(0,2).join(' '));
+}
+
+smallestTwoNumbers([30, 15, 50, 5]);
+smallestTwoNumbers([3, 0, 10, 4, 7, 3]);
