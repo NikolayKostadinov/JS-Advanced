176513840a5d4dbda857f6a4f5263a3629d10fd3
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-15 11:58:09 +0300

    add demo.js

--- 03-Arrays-and-Nested-Arrays/src/demo.js
+++ 03-Arrays-and-Nested-Arrays/src/demo.js
@@ -1,23 +1,5 @@
+const myArray = [10,20,30];
 
-let arr = ["Hiunday", "BMW", "Mercedes"];
+console.log(myArray);
 
-arr.splice(2, 0, "Ford");
-
-console.log(arr);
-
-console.log(arr.splice(2, 1, "Trabant"));
-
-arr.sort((a, b) => a.localeCompare(b));
-
-console.log(arr);
-
-let half = arr.slice(0, 2);
-console.log(half);
-
-for(let car of arr.filter(c=>c.length>4)){
-    console.log(car);
-}
-
-let nums = [1, 2, 3, 4, 5];
-let sum = nums.reduce((acc, curr)=>acc+=curr);
-console.log(sum);
+console.log(myArray.length);
