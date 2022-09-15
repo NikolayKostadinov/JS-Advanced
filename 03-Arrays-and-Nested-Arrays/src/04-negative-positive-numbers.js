6cc988f43edc90842867e88f847e249400c06dca
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-15 14:53:00 +0300

    add 04-negative-positive-numbers.js

---
+++ 03-Arrays-and-Nested-Arrays/src/04-negative-positive-numbers.js
@@ -0,0 +1,17 @@
+function negPosNumbers(arr){
+    const result = [];
+
+    for (const num of arr) {
+        if  (num < 0){
+            result.unshift(num);
+        } else {
+            result.push(num);
+        }
+    }
+
+    return console.log(result.join('\n'));
+}
+
+negPosNumbers([7, -2, 8, 9]);
+console.log('--------------------')
+negPosNumbers([3, -2, 0, -1]);
