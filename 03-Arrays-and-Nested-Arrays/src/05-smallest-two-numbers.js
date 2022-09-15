99848d0e8bf8701cde864ed084c2aebcb62af6f6
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-15 15:15:09 +0300

    add 06-bigger-half

--- 03-Arrays-and-Nested-Arrays/src/05-smallest-two-numbers.js
+++ 03-Arrays-and-Nested-Arrays/src/05-smallest-two-numbers.js
@@ -1,5 +1,8 @@
 function smallestTwoNumbers(arr){
-    console.log(arr.sort((a,b) => Number(a) -Number(b)).splice(0,2).join(' '));
+    console.log(
+        arr.sort((a,b) => Number(a) -Number(b))
+        .splice(0,2)
+        .join(' '));
 }
 
 smallestTwoNumbers([30, 15, 50, 5]);
---
+++ 03-Arrays-and-Nested-Arrays/src/06-bigger-half.js
@@ -0,0 +1,9 @@
+function biggerHalf(arr) {
+    let startIx = Math.floor(arr.length / 2);
+    return arr
+        .sort((a, b) => Number(a) - Number(b))
+        .splice(startIx);
+}
+
+console.log(biggerHalf([4, 7, 2, 5]));
+console.log(biggerHalf([3, 19, 14, 7, 2, 19, 6]));
