028406dca747f575a2d7bb44dc8fff20a4ade4d2
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-16 15:36:16 +0300

    add 09-magic-matrices.js refactored

--- 04-Arrays-and-Nested-Arrays-Exercise/src/09-magic-matrices.js
+++ 04-Arrays-and-Nested-Arrays-Exercise/src/09-magic-matrices.js
@@ -2,38 +2,33 @@ function magicMatrices(mat) {
     const sum = (arr) => arr.reduce((p, c) => p + c);
     const targetSum = sum(mat[0]);
     for (let i = 0; i < mat.length; i++) {
-        if (sum(mat[i]) != targetSum) {
-            console.log('false');
-            return;
-        }
+        if (sum(mat[i]) != targetSum) return false;
 
         let colSum = 0;
-
         for (let j = 0; j < mat.length; j++) {
             colSum += mat[j][i];
         }
 
-        if (colSum != targetSum) {
-            console.log('false');
-            return;
-        }
+        if (colSum != targetSum) return false;
     }
-    console.log('true');
+
+    return true;
 }
 
-magicMatrices(
+console.log(magicMatrices(
     [[4, 5, 6],
     [6, 5, 4],
     [5, 5, 5]]
-);
+));
 
-magicMatrices(
+console.log(magicMatrices(
     [[11, 32, 45],
     [21, 0, 1],
     [21, 1, 1]]
-);
-magicMatrices(
+));
+
+console.log(magicMatrices(
     [[1, 0, 0],
     [0, 0, 1],
     [0, 1, 0]]
-);
+));
