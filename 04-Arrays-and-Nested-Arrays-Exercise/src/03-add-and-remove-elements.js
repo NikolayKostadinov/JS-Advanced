3f595e6c2a53ae5c5ff69763b3e7693bda254b1f
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-16 14:22:03 +0300

    add 03-add-and-remove-elements.js

--- 04-Arrays-and-Nested-Arrays-Exercise/src/03-add-and-remove-elements.js
+++ 04-Arrays-and-Nested-Arrays-Exercise/src/03-add-and-remove-elements.js
@@ -1,21 +1,31 @@
-function addAndRemoveElements(){
-
+function addAndRemoveElements(commands) {
+    const arr = [];
+    let ix = 0;
+    commands.forEach(element => {
+        ix++;
+        if (element === 'add') {
+            arr.push(ix);
+        } else {
+            arr.pop();
+        }
+    });
+    console.log(arr.length ? arr.join('\n') : 'Empty');
 }
 
-addAndRemoveElements(['add', 
-'add', 
-'add', 
-'add']
+addAndRemoveElements(['add',
+    'add',
+    'add',
+    'add']
 );
 
-addAndRemoveElements(['add', 
-'add', 
-'remove', 
-'add', 
-'add']
+addAndRemoveElements(['add',
+    'add',
+    'remove',
+    'add',
+    'add']
 );
 
-addAndRemoveElements(['remove', 
-'remove', 
-'remove']
+addAndRemoveElements(['remove',
+    'remove',
+    'remove']
 );
