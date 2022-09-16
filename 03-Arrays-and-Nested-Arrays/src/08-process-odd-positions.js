2431c3688a24f6df168dd05c99fdb965ef8b5012
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-16 10:00:13 +0300

    add functional solution to 08-process-odd-positions.js

--- 03-Arrays-and-Nested-Arrays/src/08-process-odd-positions.js
+++ 03-Arrays-and-Nested-Arrays/src/08-process-odd-positions.js
@@ -8,5 +8,19 @@ function processOdd(arr) {
     console.log(result.reverse().join(' '));
 }
 
+// functional variant
+
+function processOddFunctional(arr) {
+    console.log(arr
+        .filter((e, ix) => ix % 2)
+        .map(element => element * 2)
+        .reverse()
+        .join(' '));
+}
+
+
 processOdd([10, 15, 20, 25]);
 processOdd([3, 0, 10, 4, 7, 3]);
+
+processOddFunctional([10, 15, 20, 25]);
+processOddFunctional([3, 0, 10, 4, 7, 3]);
---
+++ 03-Arrays-and-Nested-Arrays/src/09-biggest-element.js
@@ -0,0 +1,10 @@
+function biggestElement(matrix) {
+
+}
+
+biggestElement([[20, 50, 10],
+                [8, 33, 145]]);
+
+biggestElement([[3, 5, 7, 12],
+    [-1, 4, 33, 2],
+    [8, 3, 0, 4]]);
