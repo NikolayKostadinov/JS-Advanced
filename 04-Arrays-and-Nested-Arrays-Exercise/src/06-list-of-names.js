1426b66d2919fc4a96b0ee624f042d69174cea8d
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-16 12:24:06 +0300

    add 01-print-an-array-with-a-given-delimiter.js

--- 04-Arrays-and-Nested-Arrays-Exercise/src/06-list-of-names.js
+++
@@ -1,8 +0,0 @@
-function solve(arr){
-    let ix=1;
-    return arr.sort((a,b)=>a.localeCompare(b))
-    .map(a => ix++ + '.' + a)
-    .join("\n");
-}
-
-console.log(solve(["John", "Bob", "Christina", "Ema"]));
