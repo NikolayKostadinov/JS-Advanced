26fb1f504915f5fd6e3ba4130e81142358bc9927
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-16 14:39:14 +0300

    add 05-extract-increasing-subset-from-array.js

--- 04-Arrays-and-Nested-Arrays-Exercise/src/05-extract-increasing-subset-from-array.js
+++ 04-Arrays-and-Nested-Arrays-Exercise/src/05-extract-increasing-subset-from-array.js
@@ -1,1 +1,14 @@
+function extractIncreasing(arr) {
+    return arr.reduce((prev, curr) => {
+        if (prev.slice(-1) <= curr) {
+            prev.push(curr);
+        }
+        return prev;
+    }, []);
+}
 
+console.log(extractIncreasing([1, 3, 8, 4, 10, 12, 3, 2, 24]));
+
+console.log(extractIncreasing([1, 2, 3, 4]));
+
+console.log(extractIncreasing([20, 3, 2, 15, 6, 1]));
