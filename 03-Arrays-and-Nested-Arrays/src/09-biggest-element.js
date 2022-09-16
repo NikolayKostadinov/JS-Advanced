887b5138f98e2e3e48a572b7a8d95040de546c7a
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-16 10:51:24 +0300

    add functional solution to 09-bigest-element.js

--- 03-Arrays-and-Nested-Arrays/src/09-biggest-element.js
+++ 03-Arrays-and-Nested-Arrays/src/09-biggest-element.js
@@ -1,10 +1,33 @@
 function biggestElement(matrix) {
-    
+    let max = Number.MIN_SAFE_INTEGER;
+    matrix.forEach(row => {
+        row.forEach(element => max = max < element ? element : max);
+    });
+
+    console.log(max);
+}
+
+
+function biggestElementReducer(arr) {
+    console.log(arr
+        .reduce((prev, curr) => {
+            let rowMax = Math.max(...curr);
+            return prev < rowMax ? rowMax : prev;  
+        }, Number.MIN_SAFE_INTEGER));
 }
 
 biggestElement([[20, 50, 10],
-                [8, 33, 145]]);
+[8, 33, 145]]);
 
 biggestElement([[3, 5, 7, 12],
-    [-1, 4, 33, 2],
-    [8, 3, 0, 4]]);
+[-1, 4, 33, 2],
+[8, 3, 0, 4]]);
+
+console.log('------------------');
+
+biggestElementReducer([[20, 50, 10],
+[8, 33, 145]]);
+
+biggestElementReducer([[3, 5, 7, 12],
+[-1, 4, 33, 2],
+[8, 3, 0, 4]]);
