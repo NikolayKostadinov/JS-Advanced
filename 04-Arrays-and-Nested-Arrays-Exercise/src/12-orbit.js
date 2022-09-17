323b8d1e2c44dfeebb23c966f320f849dcc33aa0
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-17 14:51:03 +0300

    add 12-orbit.js

---
+++ 04-Arrays-and-Nested-Arrays-Exercise/src/12-orbit.js
@@ -0,0 +1,15 @@
+function orbit(input) {
+    let [cols, rows, starRow, starCol] = input;
+
+    let matrix = Array(rows).fill(0).map(() => Array(cols).fill(0));
+
+    for(let row = 0; row< rows; row++) {
+        for(let col=0; col<cols; col++) {
+            matrix[row][col] = Math.max(Math.abs(row - starRow), Math.abs(col - starCol)) + 1;
+        }
+    }
+ 
+    console.log(matrix.map(row => row.join(" ")).join("\n"));
+}
+
+orbit([5, 5, 3, 3]);
