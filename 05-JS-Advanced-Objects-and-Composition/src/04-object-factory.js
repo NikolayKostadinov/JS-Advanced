93ffb0fa71583348b2c965c973dd8f7e34726f46
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-18 15:13:10 +0300

    add 04-object-factory.js

---
+++ 05-JS-Advanced-Objects-and-Composition/src/04-object-factory.js
@@ -0,0 +1,55 @@
+function factory(library, orders) {
+    let products = [];
+
+    for (const order of orders) {
+        let product = order.template;
+        for (const part of order.parts) {
+            product[part] = library[part];
+        }
+
+        products.push(product);
+    }
+    
+    return products;
+}
+
+function factoryFunctional(library, orders) {
+    return orders.map(x => {
+        const result = {};
+        result.name = x.template.name;
+        x.parts.forEach(method => result[method] = library[method]);
+        return result;
+    });
+}
+
+const library = {
+    print: function () {
+        console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+        console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+};
+const orders = [
+    {
+        template: { name: 'ACME Printer' },
+        parts: ['print']
+    },
+    {
+        template: { name: 'Initech Scanner' },
+        parts: ['scan']
+    },
+    {
+        template: { name: 'ComTron Copier' },
+        parts: ['scan', 'print']
+    },
+    {
+        template: { name: 'BoomBox Stereo' },
+        parts: ['play']
+    }
+];
+const products = factory(library, orders);
+console.log(products);
