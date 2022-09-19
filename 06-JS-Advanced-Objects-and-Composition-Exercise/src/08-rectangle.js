95508abfce03a00a507f22434d55f8415b440d3d
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-09-19 14:18:57 +0300

    add 08-rectangle.js

---
+++ 06-JS-Advanced-Objects-and-Composition-Exercise/src/08-rectangle.js
@@ -0,0 +1,16 @@
+function rectangle(width, height, color){
+    return {
+        width,
+        height,
+        color: color[0].toUpperCase() + color.slice(1),
+        calcArea(){
+            return this.height * this.width;
+        }
+    }
+}
+
+let rect = rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
