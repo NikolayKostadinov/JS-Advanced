119aba098fd5898a0921c4b7576dfc3374cbef76
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-09-19 14:09:30 +0300

    add 07-towns-toJSON.js

--- 06-JS-Advanced-Objects-and-Composition-Exercise/src/06-store-catalogue.js
+++ 06-JS-Advanced-Objects-and-Composition-Exercise/src/06-store-catalogue.js
@@ -18,7 +18,6 @@ function storeCatalogue(products) {
 
     console.log(result);
 }
-
 storeCatalogue(['Appricot : 20.4',
     'Fridge : 1500',
     'TV : 1499',
@@ -27,14 +26,3 @@ storeCatalogue(['Appricot : 20.4',
     'Apple : 1.25',
     'Anti-Bug Spray : 15',
     'T-Shirt : 10']);
-    
-    storeCatalogue(
-        ['Banana : 2',
-    'Rubic\'s Cube : 5',
-    'Raspberry P : 4999',
-    'Rolex : 100000',
-    'Rollon : 10',
-    'Rali Car : 2000000',
-    'Pesho : 0.000001',
-    'Barrel : 10']
-    );
