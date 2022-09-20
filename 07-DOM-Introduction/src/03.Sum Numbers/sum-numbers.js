0fb3e32df8351109de1f969d0236ed31b0cb0623
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-09-20 14:14:03 +0300

    add 04. Show More

--- 07-DOM-Introduction/src/03.Sum Numbers/sum-numbers.js
+++ 07-DOM-Introduction/src/03.Sum Numbers/sum-numbers.js
@@ -1,7 +1,7 @@
 function calc() {
-    const num1 = document.getElementById('num1');
-    const num2 = document.getElementById('num2');
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
     const sum = document.getElementById('sum');
 
-    sum.value = Number(num1.value) + Number(num2.value);
+    sum.value = Number(num1) + Number(num2);
 }
