3689e453873fe53e1afd00490b073cbddbbc7c98
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-09-21 09:43:48 +0300

    add 01. Substract

--- 08-DOM-Introduction - Exercise/src/01. Subtraction/subtract.js
+++ 08-DOM-Introduction - Exercise/src/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+    const num1 = document.getElementById('firstNumber').value;
+    const num2 = document.getElementById('secondNumber').value;
+    const result = Number(num1) - Number(num2);
+    document.getElementById('result').textContent = result;
 }
