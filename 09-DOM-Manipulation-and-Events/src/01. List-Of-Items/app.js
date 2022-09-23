890b52579cf6448340cb409fefd26a81f684be19
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-09-23 11:25:01 +0300

    add 01. List-Of-Items refactored

--- 09-DOM-Manipulation-and-Events/src/01. List-Of-Items/app.js
+++ 09-DOM-Manipulation-and-Events/src/01. List-Of-Items/app.js
@@ -2,6 +2,6 @@ function addItem() {
     const liElement = document.createElement('li');
     const inputElement = document.getElementById('newItemText');
     liElement.textContent = inputElement.value;
-    inputElement.value = '';
     document.getElementById('items').appendChild(liElement);
+    inputElement.value = '';
 }
