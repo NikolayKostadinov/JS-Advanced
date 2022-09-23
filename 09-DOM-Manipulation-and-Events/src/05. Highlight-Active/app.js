69fa1c632e410a2b1bf12431fa6042f5264d3d19
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-09-23 16:33:23 +0300

    refactored 05. Highlight-Active

--- 09-DOM-Manipulation-and-Events/src/05. Highlight-Active/app.js
+++ 09-DOM-Manipulation-and-Events/src/05. Highlight-Active/app.js
@@ -1,15 +1,15 @@
 function focused() {
     const inputs = Array.from(document.querySelectorAll('div input'));
-    inputs.forEach(input=>{
+    inputs.forEach(input => {
         input.addEventListener("focus", onFocus, false);
         input.addEventListener("blur", onBlur, false);
     });
 
-    function onFocus(ev){
-        ev.target.parentElement.className = 'focused';
+    function onFocus(ev) {
+        ev.target.parentElement.classList.add('focused');
     }
 
-    function onBlur(ev){
-        ev.target.parentElement.className = '';
+    function onBlur(ev) {
+        ev.target.parentElement.classList.remove('focused');
     }
 }
