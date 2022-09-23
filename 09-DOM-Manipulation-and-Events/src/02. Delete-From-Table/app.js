1c2ccfe5967fd493bdb89bc01f7840000beffa42
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-09-23 11:56:07 +0300

    add 02. Delete-From-Table

--- 09-DOM-Manipulation-and-Events/src/02. Delete-From-Table/app.js
+++ 09-DOM-Manipulation-and-Events/src/02. Delete-From-Table/app.js
@@ -1,3 +1,13 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const value = document.querySelector('input[name="email"]').value;
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+    let message = 'Not found.';
+    rows.forEach(row => {
+        if (row.children[1].textContent === value){
+            row.parentElement.removeChild(row);
+            message = "Deleted."
+        }
+    })
+
+    document.getElementById('result').textContent = message;
 }
