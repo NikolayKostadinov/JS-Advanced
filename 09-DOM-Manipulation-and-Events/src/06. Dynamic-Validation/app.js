2ca325ab0874eaede6588438d093d8abaa4bd34d
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-24 13:04:24 +0300

    add 06. Dynamic-Validation

---
+++ 09-DOM-Manipulation-and-Events/src/06. Dynamic-Validation/app.js
@@ -0,0 +1,12 @@
+function validate() {
+    const pattern = new RegExp(/^\S+@\S+\.\S+$/gm);
+
+    const inputElement  = document.getElementById('email');
+    inputElement.addEventListener('change', onChane);
+    function onChane(ev){
+        ev.target.classList.remove('error');
+        if(!pattern.test(ev.target.value)){
+            ev.target.classList.add('error');
+        }
+    }
+}
