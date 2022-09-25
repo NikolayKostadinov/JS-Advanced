7fe4738037fc1f2c6af1b7793f51b8bd0e6bc47a
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-25 13:24:49 +0300

    add 02. Time converter

--- 10-DOM-Manipulation-and-Events-Exercise/src/02. Time-Converter/solution.js
+++ 10-DOM-Manipulation-and-Events-Exercise/src/02. Time-Converter/solution.js
@@ -4,36 +4,34 @@ function attachEventsListeners() {
     const minutesElement = document.getElementById('minutes');
     const secondsElement = document.getElementById('seconds');
 
-    document.getElementById("daysBtn").addEventListener("click", onDaysBtnClick);
-    document.getElementById("hoursBtn").addEventListener("click", onHoursBtnClick);
-    document.getElementById("minutesBtn").addEventListener("click", onMinutesBtnClick);
-    document.getElementById("secondsBtn").addEventListener("click", onSecondsBtnClick);
+    document.getElementById("daysBtn").addEventListener("click", onConvertClick);
+    document.getElementById("hoursBtn").addEventListener("click", onConvertClick);
+    document.getElementById("minutesBtn").addEventListener("click", onConvertClick);
+    document.getElementById("secondsBtn").addEventListener("click", onConvertClick);
 
-    function onDaysBtnClick() {
-        const days = Number(daysElement.value);
-        hoursElement.value = days * 24;
-        minutesElement.value = days * 1440;
-        secondsElement.value = days * 86400;
+    const ratios = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400
     }
 
-    function onHoursBtnClick() {
-        const hours = Number(hoursElement.value);
-        daysElement.value = hours / 24;
-        minutesElement.value = hours * 60;
-        secondsElement.value = hours * 60 * 60;
+    function convert(value, unit) {
+        let days = value / ratios[unit];
+        return {
+            days: days,
+            hours: days * ratios.hours,
+            minutes: days * ratios.minutes,
+            seconds: days * ratios.seconds
+        }
     }
 
-    function onMinutesBtnClick() {
-        const minutes = Number(minutesElement.value);
-        daysElement.value = minutes / 24 / 60;
-        hoursElement.value = minutes / 60;
-        secondsElement.value = minutes * 60;
-    }
-
-    function onSecondsBtnClick() {
-        const seconds = Number(secondsElement.value);
-        daysElement.value = seconds / 24 / 60 / 60;
-        hoursElement.value = seconds / 60 / 60;
-        minutesElement.value = seconds / 60 ;
+    function onConvertClick(ev) {
+        const input = ev.target.parentElement.querySelector('input[type="text"]');
+        const time = convert(Number(input.value), input.id);
+        daysElement.value = time.days;
+        hoursElement.value = time.hours;
+        minutesElement.value = time.minutes;
+        secondsElement.value = time.seconds;
     }
 }
