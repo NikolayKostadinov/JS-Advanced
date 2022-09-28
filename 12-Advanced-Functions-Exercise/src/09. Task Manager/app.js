76f1b42cac183b6337bc77ec0f697c14f6677745
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-28 18:33:52 +0300

    refactored 09. Task Manager

--- 12-Advanced-Functions-Exercise/src/09. Task Manager/app.js
+++ 12-Advanced-Functions-Exercise/src/09. Task Manager/app.js
@@ -1,10 +1,11 @@
 function solve() {
-    const colOpen = document.querySelector('h1.orange').parentNode.parentNode.children[1];
-    const colInProgress = document.querySelector('h1.yellow').parentNode.parentNode.children[1];
-    const colComplete = document.querySelector('h1.green').parentNode.parentNode.children[1];
     const task = document.getElementById('task');
     const description = document.getElementById('description');
     const date = document.getElementById('date');
+
+    const [_,colOpen, colInProgress, colComplete] =
+        Array.from(document.querySelectorAll('section'))
+            .map(s=>s.children[1]);
 
     document.getElementById('add').addEventListener('click', addTask);
 
@@ -17,54 +18,43 @@ function solve() {
         return element
     }
 
-    function deleteHandler(ev) {
-        ev.target.parentElement.parentElement.remove();
-    }
-
-    function finishHandler(ev) {
-        const article = ev.target.parentElement.parentElement;
-        const btnWrapper = article.querySelector('div.flex');
-        btnWrapper.remove();
-        colComplete.appendChild(article);
-    }
-
-    function startHandler(ev) {
-        const article = ev.target.parentElement.parentElement;
-        
-        const btnWrapper = article.querySelector('div.flex')
-        btnWrapper.querySelector('button').remove();
-
-        const btnFinish = createElement('button', 'Finish', 'orange');
-        btnFinish.addEventListener('click', finishHandler);
-
-        btnWrapper.appendChild(btnFinish);
-        colInProgress.appendChild(article);
-    }
-
-    function createButtons() {
-        const div = document.createElement('div');
-        div.classList.add('flex');
-
-        const btnStart = createElement('button', 'Start', 'green');
-        btnStart.addEventListener('click', startHandler);
-
-        const btnDelete = createElement('button', 'Delete', 'red');
-        btnDelete.addEventListener('click', deleteHandler);
-
-        div.appendChild(btnStart);
-        div.appendChild(btnDelete);
-
-        return div;
-    }
-
     function addTask(ev) {
         ev.preventDefault();
         const article = document.createElement('article');
         article.appendChild(createElement('h3', task.value));
         article.appendChild(createElement('p', `Description: ${description.value}`));
         article.appendChild(createElement('p', `Due Date: ${date.value}`));
-        article.appendChild(createButtons());
 
+        const btnWrapper = document.createElement('div');
+        btnWrapper.classList.add('flex');
+
+        const btnStart = createElement('button', 'Start', 'green');
+        const btnDelete = createElement('button', 'Delete', 'red');
+        const btnFinish = createElement('button', 'Finish', 'orange');
+
+        btnStart.addEventListener('click', startHandler);
+        btnDelete.addEventListener('click', deleteHandler);
+        btnFinish.addEventListener('click', finishHandler);
+
+        btnWrapper.appendChild(btnStart);
+        btnWrapper.appendChild(btnDelete);
+
+        article.appendChild(btnWrapper);
         colOpen.appendChild(article);
+
+        function startHandler() {
+            btnStart.remove();
+            btnWrapper.appendChild(btnFinish);
+            colInProgress.appendChild(article);
+        }
+
+        function deleteHandler() {
+            article.remove();
+        }
+
+        function finishHandler() {
+            btnWrapper.remove();
+            colComplete.appendChild(article);
+        }
     }
 }
