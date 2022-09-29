6d22bbcfed9fc7c80c0e2005275eccc918764b34
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-09-29 14:39:04 +0300

    refactored 03-deck-of-cards.js

--- 13-Unit-Testing-and- Error-Handling/src/03-deck-of-cards.js
+++ 13-Unit-Testing-and- Error-Handling/src/03-deck-of-cards.js
@@ -1,29 +1,35 @@
 function solve(cards) {
-    const suits = {
-        S: '\u2660',
-        H: '\u2665',
-        D: '\u2666',
-        C: '\u2663'
-    };
 
-    const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
     const deck = [];
-    try {
-        for (const card of cards) {
-            const face = card.slice(0, -1);
-            const suit = card.slice(-1);
+
+    for (const card of cards) {
+        const face = card.slice(0, -1);
+        const suit = card.slice(-1);
+        try {
             const cardObj = createCard(face, suit)
             deck.push(cardObj);
+        } catch (ex) {
+            console.log(`Invalid card: ` + card);
+            return;
         }
-        console.log(deck.join(' '));
-    } catch (ex) {
-        console.log(ex.message);
     }
+    console.log(deck.join(' '));
 
 
     function createCard(face, suit) {
-        if (faces.indexOf(face) === -1 || !suits.hasOwnProperty(suit)) throw Error(`Invalid card: ${face + suit}`);
+        const suits = {
+            S: '\u2660',
+            H: '\u2665',
+            D: '\u2666',
+            C: '\u2663'
+        };
+        const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+
+        if (faces.indexOf(face) == -1) throw Error(`Invalid face ${face}!`);
+        if (!suits.hasOwnProperty(suit)) throw Error(`Invalid suit ${suit}!`);
+
         return {
             face,
             suit: suits[suit],
