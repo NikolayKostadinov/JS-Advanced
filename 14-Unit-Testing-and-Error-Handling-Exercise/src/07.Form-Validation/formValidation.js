b579bb986f2281bea34120ba2a13f57d0256ddd5
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-09-30 18:39:11 +0300

    add 07. Form Validation

--- 14-Unit-Testing-and-Error-Handling-Exercise/src/07.Form-Validation/formValidation.js
+++ 14-Unit-Testing-and-Error-Handling-Exercise/src/07.Form-Validation/formValidation.js
@@ -5,6 +5,7 @@ function validate() {
     const confirmPassword = document.getElementById('confirm-password');
     const companyNumber = document.getElementById('companyNumber');
     const company = document.getElementById('company');
+    const errFields = [];
 
     company.addEventListener('change', () => {
         document.getElementById('companyInfo').style.display =
@@ -16,51 +17,49 @@ function validate() {
 
     function isCorrectUserName(username) {
         let userNamePattern = /^[a-zA-Z0-9]{3,20}$/gm;
-        return username && userNamePattern.test(username);
+        return userNamePattern.test(username);
     }
 
     function isCorrectEmail(email) {
-        const emailPattern = /^[^@.]+@[^@]*\.[^@]+$/;
+        const emailPattern = /^\w+@\w*\.+/;
         return emailPattern.test(email);
     }
 
     function isCorrectPassword(password) {
-        let regExp = /^\w{5,15}$/gm;
+        let regExp = /^\w{5,15}$/;
         return regExp.test(password) && password.localeCompare(confirmPassword.value) === 0;
     }
 
     function isCorrectCompanyInfo(companyNumber) {
-        let regExp = /^[0-9]{4}$/gm;
+        let regExp = /^[0-9]{4}$/;
         return regExp.test(companyNumber);
     }
 
     function validateFields(ev) {
         ev.preventDefault();
-        let isValid = true;
+
         if (!isCorrectUserName(username.value)) {
-            username.style.borderColor = 'red';
-            isValid = false;
+            errFields.push(username);
         }
 
         if (!isCorrectEmail(email.value)) {
-            email.style.borderColor = 'red';
-            isValid = false;
+            errFields.push(email);
         }
 
         if (!isCorrectPassword(password.value) || password.value !== confirmPassword.value) {
-            password.style.borderColor = 'red';
-            confirmPassword.style.borderColor = 'red';
-            isValid = false;
+            errFields.push(password);
+            errFields.push(confirmPassword);
         }
 
         if (company.checked) {
             if (!isCorrectCompanyInfo(companyNumber.value)) {
-                companyNumber.style.borderColor = 'red';
-                isValid = false;
+                errFields.push(companyNumber);
             }
         }
 
+        errFields.forEach(field => field.style.borderColor = 'red');
+
         document.getElementById('valid').style.display =
-            isValid ? 'block' : 'none';
+            errFields.length ? 'none' : 'block';
     }
 }
