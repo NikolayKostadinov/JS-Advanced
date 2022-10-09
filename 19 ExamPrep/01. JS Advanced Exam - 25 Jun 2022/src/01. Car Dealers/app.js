11a355db4d7eb2b9ae4d5fec1390828ac5dd7d67
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-10-09 17:32:12 +0300

    add 01.Forum post

--- 19 ExamPrep/01. JS Advanced Exam - 25 Jun 2022/src/01. Car Dealers/app.js
+++ 19 ExamPrep/01. JS Advanced Exam - 25 Jun 2022/src/01. Car Dealers/app.js
@@ -1,1 +1,90 @@
 window.addEventListener("load", solve);
+
+function solve() {
+    const inputs = {
+        make: document.getElementById('make'),
+        model: document.getElementById('model'),
+        year: document.getElementById('year'),
+        fuel: document.getElementById('fuel'),
+        originalCost: document.getElementById('original-cost'),
+        sellingPrice: document.getElementById('selling-price')
+    }
+
+    let profit = 0;
+
+    document.getElementById('publish').addEventListener('click', onClick);
+
+    function onClick(ev) {
+        ev.preventDefault();
+        if (isValid()) {
+            addRowToTable();
+            clearAll();
+        }
+    }
+
+    function addRowToTable() {
+        const data = Object.fromEntries(Object.entries(inputs)
+            .map(([name, input]) => [name, input.value])
+        );
+
+        const tableBody = document.getElementById('table-body');
+        const tr = createElement('tr', null, 'row');
+        tr.appendChild(createElement('td', inputs.make.value));
+        tr.appendChild(createElement('td', inputs.model.value));
+        tr.appendChild(createElement('td', inputs.year.value));
+        tr.appendChild(createElement('td', inputs.fuel.value));
+        tr.appendChild(createElement('td', inputs.originalCost.value));
+        tr.appendChild(createElement('td', inputs.sellingPrice.value));
+        const tdAction = createElement('td');
+        const editBtn = createElement('button', 'Edit', 'action-btn edit');
+        const sellBtn = createElement('button', 'Sell', 'action-btn sell');
+        tdAction.appendChild(editBtn);
+        tdAction.appendChild(sellBtn);
+        tr.appendChild(tdAction);
+        tableBody.appendChild(tr);
+
+        editBtn.addEventListener('click', (ev) => {
+            tr.remove();
+            Object.entries(inputs)
+                .forEach(([name, input]) => {
+                    input.value = data[name];
+                })
+        });
+
+        sellBtn.addEventListener('click', (ev) => {
+            tr.remove();
+            const ul = document.getElementById('cars-list');
+            const li = createElement('li', null, 'each-list');
+            li.appendChild(createElement('span', data.make + ' ' + data.model));
+            li.appendChild(createElement('span', data.year));
+            li.appendChild(createElement('span', Number(data.sellingPrice) - Number(data.originalCost)));
+            ul.appendChild(li);
+            profit += Number(data.sellingPrice) - Number(data.originalCost);
+
+            document.getElementById('profit').textContent = profit.toFixed(2);
+        });
+    }
+
+    function createElement(type, text, cssClass) {
+        const element = document.createElement(type);
+        if (text) {
+            element.textContent = text;
+        }
+
+        if (cssClass) {
+            element.className = cssClass;
+        }
+        return element;
+    }
+
+    function isValid() {
+        return Object
+                .values(inputs)
+                .some(input => input && input.value && input.value !== '')
+            && Number(inputs.originalCost.value) <= Number(inputs.sellingPrice.value);
+    }
+
+    function clearAll() {
+        Object.values(inputs).forEach(input => input.value = '');
+    }
+}
--- 19 ExamPrep/02. JS Advanced Retake Exam - 6 Apr 2022/src/01. Forum posts/app.js
+++ 19 ExamPrep/02. JS Advanced Retake Exam - 6 Apr 2022/src/01. Forum posts/app.js
@@ -1,5 +1,78 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ...
+    const inputs = {
+        title: document.getElementById('post-title'),
+        category: document.getElementById('post-category'),
+        content: document.getElementById('post-content'),
+    }
+
+    document.getElementById('publish-btn')
+        .addEventListener('click', (ev) => {
+            ev.preventDefault();
+            if (isValid()) {
+                fillPostReview();
+                clearAll();
+            }
+        });
+
+    function fillPostReview() {
+        const data = {};
+        Object
+            .entries(inputs)
+            .forEach(([name, input]) => data[name] = input.value);
+
+        const rpost = createElement('li', '', 'rpost');
+        const article = createElement('article');
+        article.appendChild(createElement('h4', data.title))
+        article.appendChild(createElement('p', `Category: ${data.category}`))
+        article.appendChild(createElement('p', `Content: ${data.content}`))
+        rpost.appendChild(article);
+
+        const editBtn = createElement('button', 'Edit', 'action-btn edit');
+        const approveBtn = createElement('button', 'Approve', 'action-btn approve');
+        rpost.appendChild(editBtn);
+        rpost.appendChild(approveBtn);
+
+        editBtn.addEventListener('click', () => {
+            rpost.remove();
+            Object.entries(inputs)
+                .forEach(([name, input]) => input.value = data[name]);
+        })
+
+        approveBtn.addEventListener('click', () => {
+            editBtn.remove();
+            approveBtn.remove();
+            document.getElementById('published-list')
+                .appendChild(rpost);
+        })
+        document.getElementById('review-list').appendChild(rpost);
+
+        document.getElementById('clear-btn')
+            .addEventListener('click', () => {
+                rpost.remove()
+            });
+    }
+
+    function createElement(type, text, cssClass) {
+        const element = document.createElement(type);
+        if (text) {
+            element.textContent = text;
+        }
+
+        if (cssClass) {
+            element.className = cssClass;
+        }
+        return element;
+    }
+
+    function isValid() {
+        return Object
+            .values(inputs)
+            .every(input => input.value);
+    }
+
+    function clearAll() {
+        Object.values(inputs).forEach(input => input.value = '');
+    }
 }
