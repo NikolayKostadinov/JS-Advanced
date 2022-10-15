905d2149369f203b4a589656e7e94668b54c168a
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-10-15 10:33:45 +0300

    Initial Commit 08. JS Advanced Retake Exam - 10 Aug 2022

---
+++ 19 ExamPrep/08.  JS Advanced Retake Exam - 10 Aug 2022/src/utility_functions.js
@@ -0,0 +1,39 @@
+function setInputsData(data) {
+    Object.entries(inputs)
+        .forEach(([name, input]) => input.value = data[name]);
+}
+
+function getInputs() {
+    return Object.fromEntries(
+        Array.from(document.querySelectorAll('input, textarea, select'))
+            .map(i => [i.id, i]));
+}
+
+function createElement(type, text, cssClass) {
+    const element = document.createElement(type);
+    if (text) {
+        element.textContent = text;
+    }
+
+    if (cssClass) {
+        element.className = cssClass;
+    }
+    return element;
+}
+
+function getData() {
+    return Object.fromEntries(
+        Object.entries(inputs)
+            .map(([name, input]) => [name, input.value])
+    );
+}
+
+function isValid() {
+    return Object.values(inputs)
+            .every(i => i.value)
+        &&(inputs['type-product'].value === 'Computer' || inputs['type-product'].value === 'Phone');
+}
+
+function clearInput() {
+    Object.values(inputs).forEach(i => i.value = '');
+}
