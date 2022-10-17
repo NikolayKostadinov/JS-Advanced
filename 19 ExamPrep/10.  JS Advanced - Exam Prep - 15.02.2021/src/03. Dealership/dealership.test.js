7c8545aa1acee84d9d7e55c5d52b2bd37db8f136
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-10-17 11:04:02 +0300

    Initial commit for 10.  JS Advanced - Exam Prep - 15.02.2021

---
+++ 19 ExamPrep/10.  JS Advanced - Exam Prep - 15.02.2021/src/03. Dealership/dealership.test.js
@@ -0,0 +1,12 @@
+const {assert} = require('chai');
+const dealership = require('./dealership');
+describe("Tests …", function() {
+    describe("TODO …", function() {
+
+        it("TODO …", function() {
+            // TODO: …
+        });
+    });
+
+    // TODO: …
+});
