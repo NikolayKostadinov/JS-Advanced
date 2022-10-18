3c9930cb0568e026a0ee55483c82b2e847889f50
NikolayKostadinov <kostadinov.nikolay.d@gmail.com>
2022-10-18 16:12:18 +0300

    add 02. Repository

--- 19 ExamPrep/11. JS Advanced Retake - 08 April 2020/src/03. Bank/solution.js
+++ 19 ExamPrep/11. JS Advanced Retake - 08 April 2020/src/03. Bank/solution.js
@@ -74,16 +74,3 @@ class Bank {
         return result.join('\n').trim();
     }
 }
-
-// let bank = new Bank('SoftUni Bank');
-//
-// console.log(bank.newCustomer({firstName: 'Svetlin', lastName: 'Nakov', personalId: 6233267}));
-// console.log(bank.newCustomer({firstName: 'Mihaela', lastName: 'Mileva', personalId: 4151596}));
-//
-// bank.depositMoney(6233267, 250);
-// console.log(bank.depositMoney(6233267, 250));
-// bank.depositMoney(4151596, 555);
-//
-// console.log(bank.withdrawMoney(6233267, 125));
-//
-// console.log(bank.customerInfo(6233267));
